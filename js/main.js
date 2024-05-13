c0c3b139364f61151cf0318650e2b7d8c0713705
Florence <22489162+flsqqq@users.noreply.github.com>
2024-05-13 23:35:43 +0800

    enable adding new sharing plates after the last sharing plate, and changed method of creating new elements to createElement()

--- js/main.js
+++ js/main.js
@@ -92,53 +92,88 @@ function addDiner(numOfDiners) {
     dinersList.appendChild(currDinerSection);
 }
 
-function plusSharedPlate(numOfSharingPlates) {
-    newSharingPlateDiv = `
-    <!-- item -->
-    <div class="item" id="item-${numOfSharingPlates}">
+function plusSharedPlate(clickedSharingPlusID) {
+    // sharing list
+    const sharingListDiv = document.querySelector(".sharing-list");
 
-        <!-- food name -->
-        <div class="food-name">
-            <input type="text" placeholder="Food">
-        </div>
-        <!-- /food name -->
+    // item
+    const newSharingPlateDiv = document.createElement("div");
+    newSharingPlateDiv.className = "item";
+    newSharingPlateDiv.id = `item ${clickedSharingPlusID + 1}`;
 
-        <!-- price -->
-        <div class="price">
-            <input type="text" placeholder="Price*">
-        </div>
-        <div class="plus-minus-sharing">
-            <div class="circle-btn sharing-plus sharing-${numOfSharingPlates}"><img src="images/icon-plus.svg" alt=""></div>
-            <div class="circle-btn sharing-minus disabled sharing-${numOfSharingPlates}"><img src="images/icon-minus.svg" alt=""></div>
-        </div>
-        <!-- /price -->
+    // food name
+    const foodNameDiv = document.createElement("div");
+    foodNameDiv.className = "food-name";
+    // input
+    const foodNameInput = document.createElement("input");
+    foodNameInput.type = "text";
+    foodNameInput.placeholder = "Food";
+    foodNameDiv.appendChild(foodNameInput);
 
-    </div>
-    <!-- item -->
-    `;
+    // price
+    const foodPriceDiv = document.createElement("div");
+    foodPriceDiv.className = "price";
+    // input
+    const foodPriceInput = document.createElement("input");
+    foodPriceInput.type = "text";
+    foodPriceInput.placeholder = "Price";
+    foodPriceDiv.appendChild(foodPriceInput);
 
-    sharingList.innerHTML += newSharingPlateDiv;
+    // plus minus sharing
+    const plusMinusSharingBtnsDiv = document.createElement("div");
+    plusMinusSharingBtnsDiv.className = "plus-minus-sharing";
+    // plus button
+    const plusSharingBtn = document.createElement("div");
+    plusSharingBtn.className = `circle-btn sharing-plus sharing-${clickedSharingPlusID + 1}`;
+    const plusSharingImg = document.createElement("img");
+    plusSharingImg.src = "images/icon-plus.svg";
+    plusSharingImg.alt = "";
+    plusSharingBtn.appendChild(plusSharingImg);
+    // minus button
+    const minusSharingBtn = document.createElement("div");
+    minusSharingBtn.className = `circle-btn sharing-minus sharing-${clickedSharingPlusID + 1}`;
+    const minusSharingImg = document.createElement("img");
+    minusSharingImg.src = "images/icon-minus.svg";
+    minusSharingImg.alt = "";
+    minusSharingBtn.appendChild(minusSharingImg);
+    // append plus and minus to the div
+    plusMinusSharingBtnsDiv.appendChild(plusSharingBtn);
+    plusMinusSharingBtnsDiv.appendChild(minusSharingBtn);
 
+    newSharingPlateDiv.appendChild(foodNameDiv);
+    newSharingPlateDiv.appendChild(foodPriceDiv);
+    newSharingPlateDiv.appendChild(plusMinusSharingBtnsDiv);
+
+    // append new sharing plate to sharing list
+    sharingListDiv.appendChild(newSharingPlateDiv);
+
+    // enable minus buttons
     const minusBtns = document.querySelectorAll(".circle-btn.sharing-minus");
-    
     minusBtns.forEach((minusBtn) => {
         if (numOfSharingPlates > 1) {
             minusBtn.classList.remove("disabled");
         } else {
             minusBtn.classList.add("disabled");
         }
-        console.log(minusBtn);
     });
 }
+
+// function minusSharedPlates() {
+
+// }
 
 // document.addEventListener("input", () => {
 // })
 
 document.addEventListener("click", (event) => {
-    // click on plus shared plate button
-    if (event.target.classList.contains("sharing-plus") || event.target.parentNode.classList.contains("sharing-plus")) {
+    
+    if (    // click on plus shared plate button
+        event.target.classList.contains("sharing-plus") ||
+        event.target.parentNode.classList.contains("sharing-plus")
+    ) {
         numOfSharingPlates++;
-        plusSharedPlate(numOfSharingPlates);
+        const clickedSharingPlusID = parseInt(event.target.className.split("sharing-").pop());
+        plusSharedPlate(clickedSharingPlusID);
     }
 })
 
